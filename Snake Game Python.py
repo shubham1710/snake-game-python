ba716c54c34015d1db3c038d472dc655b3ec582b
Kumar Shubham <shubhamstudent5@gmail.com>
2020-06-28 12:37:15 +0530

    Update Snake Game Python.py

--- Snake Game Python.py
+++ Snake Game Python.py
@@ -21,7 +21,7 @@ dis_width = 800
 dis_height = 600
  
 dis = pygame.display.set_mode((dis_width, dis_height))
-pygame.display.set_caption('Snake Game by Shubham')
+pygame.display.set_caption('Shubham's Snake Game')
  
 clock = pygame.time.Clock()
  
